bf5d428f4782cbbf2564eb6e2f01f8f19e71a56c
Jose B. E. Vallecer <ZayZay@joses-mbp.attlocal.net>
2017-03-30 23:47:11 -0400

    Buggy bug test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,7 +10,11 @@ $(document).ready(function(){
 		$.getJSON('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + selected, function(data) {
      	$('#products').empty();
      		$.each(data, function(i, item) {
-     			$('#products').append('<p class=\'cat\'>' + data[i].name + '</p>', '<img src=\'' + data[i].image_link + '\'>')
+     			$('#products').append('<div class=\'card-image waves-effect waves-block waves-light\'>' + '<img src=\'' + data[i].image_link + '\'>' + '</div>' + '<div class=\'card-content\'>' + 
+		      	'<span class=\'card-title activator grey-text text-darken-4 name\'>' + data[i].name + '<i class=\'material-icons right\'>' + 'visibility' + '</i>' + '</span>' +
+		    	'</div>' + '<div class=\'card-reveal\'>' + '<span class=\'card-title grey-text text-darken-4 name\'>' + '<i class=\'material-icons right\'>' + 'close' + '</i>' + '</span>' + '<p>' + data[i].description + '</p>' +
+		    	'</div>')
+     			//$('#products').append('<p class=\'cat\'>' + data[i].name + '</p>', '<img src=\'' + data[i].image_link + '\'>')
         /*if (i == 10) {
         	return false;
         }*/
