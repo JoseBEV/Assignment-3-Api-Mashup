5481f6b9a47d6c9196b7b4b2367e1fe99b008948
Jose B. E. Vallecer <ZayZay@joses-mbp.attlocal.net>
2017-03-30 21:52:06 -0400

    1st API implemented

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,62 @@
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function(){
+
+	var selected;
+
+	$('#eyebrow').click(function() {
+		selected = 'eyebrow';
+		console.log(selected);
+		$.getJSON('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + selected, function(data) {
+     	$('#products').empty();
+     		$.each(data, function(i, item) {
+     			$('#products').append('<p class=\'cat\'>' + data[i].name + '</p>', '<img src=\'' + data[i].image_link + '\'>')
+        /*if (i == 10) {
+        	return false;
+        }*/
+            });
+  		});   	
+	});
+	
+	$('#eyeshadow').click(function() {
+		selected = 'eyeshadow';
+		console.log(selected);
+		$.getJSON('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + selected, function(data) {
+     	$('#products').empty();
+     		$.each(data, function(i, item) {
+     			$('#products').append('<p class=\'cat\'>' + data[i].name + '</p>', '<img src=\'' + data[i].image_link + '\'>')
+        /*if (i == 10) {
+        	return false;
+        }*/
+            });
+  		});   	
+	});
+
+$('#eyeliner').click(function() {
+		selected = 'eyeliner';
+		console.log(selected);
+		$.getJSON('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + selected, function(data) {
+     	$('#products').empty();
+     		$.each(data, function(i, item) {
+     			$('#products').append('<p class=\'cat\'>' + data[i].name + '</p>', '<img src=\'' + data[i].image_link + '\'>')
+        /*if (i == 10) {
+        	return false;
+        }*/
+            });
+  		});   	
+	});
+
+$('#mascara').click(function() {
+		selected = 'mascara';
+		console.log(selected);
+		$.getJSON('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + selected, function(data) {
+     	$('#products').empty();
+     		$.each(data, function(i, item) {
+     			$('#products').append('<p class=\'cat\'>' + data[i].name + '</p>', '<img src=\'' + data[i].image_link + '\'>')
+        /*if (i == 10) {
+        	return false;
+        }*/
+            });
+  		});   	
+	});  
+});
