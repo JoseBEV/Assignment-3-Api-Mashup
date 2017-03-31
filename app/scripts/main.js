c57cdb2c97d4e12997abd10253435370fa36381f
Jose B. E. Vallecer <ZayZay@joses-mbp.attlocal.net>
2017-03-31 02:49:18 -0400

    Styling test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,10 +10,10 @@ $(document).ready(function(){
 		$.getJSON('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + selected, function(data) {
      	$('#products').empty();
      		$.each(data, function(i, item) {
-     			$('#products').append('<div class=\'card-image waves-effect waves-block waves-light\'>' + '<img src=\'' + data[i].image_link + '\'>' + '</div>' + '<div class=\'card-content\'>' + 
-		      	'<span class=\'card-title activator grey-text text-darken-4 name\'>' + data[i].name + '<i class=\'material-icons right\'>' + 'visibility' + '</i>' + '</span>' +
-		    	'</div>' + '<div class=\'card-reveal\'>' + '<span class=\'card-title grey-text text-darken-4 name\'>' + '<i class=\'material-icons right\'>' + 'close' + '</i>' + '</span>' + '<p>' + data[i].description + '</p>' +
-		    	'</div>')
+     			$('#products').append('<div class=\'card-image waves-effect waves-block waves-light center\'>' + '<img src=\'' + data[i].image_link + '\' class=\'activator\'>' + '</div>' + '<div class=\'card-content\'>' + 
+		      	'<span class=\'card-title activator grey-text text-darken-4 center\'>' + data[i].name + //'<i class=\'material-icons right\'>' + 'visibility' + '</i>' + 
+		      	'</span>' + '<br>' +
+		    	'</div>') //+ '<div class=\'card-reveal\'>' + '<span class=\'card-title grey-text text-darken-4\'>' + '<i class=\'material-icons right\'>' + 'close' + '</i>' + '</span>' + '<p>' + data[i].description + '</p>' + '</div>')
      			//$('#products').append('<p class=\'cat\'>' + data[i].name + '</p>', '<img src=\'' + data[i].image_link + '\'>')
         /*if (i == 10) {
         	return false;
@@ -28,7 +28,7 @@ $(document).ready(function(){
 		$.getJSON('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + selected, function(data) {
      	$('#products').empty();
      		$.each(data, function(i, item) {
-     			$('#products').append('<p class=\'cat\'>' + data[i].name + '</p>', '<img src=\'' + data[i].image_link + '\'>')
+     			$('#products').append('<div class=\'card-image\'>' + '<img src=\'' + data[i].image_link + '\'>' + '<span class=\'card-title\'>' + data[i].name + '</span>' + '</div>' + '<div class=\'card-content\'>' + '<p>' + data[i].description + '</p>' + '</div' )
         /*if (i == 10) {
         	return false;
         }*/
